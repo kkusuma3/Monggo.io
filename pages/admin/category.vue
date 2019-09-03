5cebeb99c0f3128d2b20e5dc80a818867cef329e
Jefry Dewangga <jefrydco@gmail.com>
2019-09-03 21:38:11 +0700

    feat: crud for category

--- pages/admin/category.vue
+++ pages/admin/category.vue
@@ -5,7 +5,6 @@
     :is-editing="isEditing"
     :is-confirming="isConfirming"
     :is-deleting="isDeleting"
-    :is-previewing="isPreviewing"
     @trigger:add="onTriggerAdd"
     @dialog:close="onDialogClose"
     @dialog:action="onDialogAction"
@@ -13,8 +12,6 @@
     @delete:action="onDeleteAction"
     @confirm:close="onConfirmClose"
     @confirm:action="onConfirmAction"
-    @preview:close="onPreviewClose"
-    @preview:action="onPreviewClose"
   >
     <v-data-table
       :headers="headers"
@@ -23,23 +20,6 @@
       :sort-desc="[true]"
       :loading="isLoading"
     >
-      <template #item.image="{ item }">
-        <v-avatar :color="getMaterialColor(item.name)" class="ma-1">
-          <app-img
-            v-if="item.imagesMeta && item.imagesMeta.length > 0"
-            :src="item.imagesMeta[0].url"
-            :alt="item.imagesMeta[0].name"
-          />
-          <span
-            v-else=""
-            :class="{
-              'white--text': isDarkColor(getMaterialColor(item.name, true))
-            }"
-          >
-            {{ getInitials(item.name) }}
-          </span>
-        </v-avatar>
-      </template>
       <template #item.createdAt="{ item }">
         <time :datetime="item.createdAt">
           {{ $moment(item.createdAt).format('llll') }}
@@ -96,65 +76,6 @@
         :label="$t('name')"
         outlined=""
       />
-      <v-textarea
-        v-model="item.description"
-        v-validate="'required'"
-        :error-messages="errors.collect('description')"
-        :disabled="isLoading"
-        data-vv-name="description"
-        :data-vv-as="$t('description')"
-        name="description"
-        clearable=""
-        data-vv-value-path="item.description"
-        required=""
-        :label="$t('description')"
-        outlined=""
-        auto-grow=""
-      />
-      <v-file-input
-        v-model="item.images"
-        v-validate="'required'"
-        :error-messages="errors.collect('images')"
-        :disabled="isLoading"
-        :prepend-icon="null"
-        data-vv-name="images"
-        :data-vv-as="$tc('image', 2)"
-        name="images"
-        counter=""
-        clearable=""
-        data-vv-value-path="item.images"
-        required=""
-        :label="$tc('image', 2)"
-        outlined=""
-        multiple=""
-        :show-size="1000"
-      >
-        <template #selection="{ index, text }">
-          <v-chip v-if="index < 2" label="" small="">
-            {{ text }}
-          </v-chip>
-
-          <span
-            v-else-if="index === 2"
-            class="overline grey--text text--darken-3 mx-2"
-          >
-            +{{ item.images.length - 2 }}
-            {{ $tc('file', item.images.length - 2) }}
-          </span>
-        </template>
-      </v-file-input>
-      <v-row>
-        <template v-for="meta in item.imagesMeta">
-          <v-col :key="meta.url" cols="4" class="d-flex child-flex">
-            <v-card ripple="" flat="" @click="onTriggerPreview(meta)">
-              <app-img :src="meta.url" :alt="meta.name" :aspect-ratio="1" />
-            </v-card>
-          </v-col>
-        </template>
-      </v-row>
-    </template>
-    <template #preview="">
-      <app-img :src="image.url" :alt="image.name" />
     </template>
   </app-wrapper>
 </template>
@@ -165,13 +86,10 @@ import uuidv4 from 'uuid/v4'
 import slugify from '@sindresorhus/slugify'
 import _cloneDeep from 'lodash.clonedeep'
 import isEqual from 'fast-deep-equal'
-import isDarkColor from 'is-dark-color'
-import materialColorHash from 'material-color-hash'
-import initials from 'initials'
 import pluralize from 'pluralize'
 import paramCase from 'param-case'
 
-import { db, storage } from '~/utils/firebase'
+import { db } from '~/utils/firebase'
 
 export default {
   layout: 'admin',
@@ -187,17 +105,9 @@ export default {
       isEditing: false,
       isConfirming: false,
       isDeleting: false,
-      isPreviewing: false,
       isSaved: false,
       headers: [
-        {
-          text: this.$tc('image'),
-          value: 'image',
-          align: 'center',
-          sortable: false
-        },
         { text: this.$t('name'), value: 'name' },
-        { text: this.$t('description'), value: 'description' },
         {
           text: this.$t('createdAt'),
           value: 'createdAt',
@@ -225,28 +135,14 @@ export default {
       item: {
         uid: uuidv4(),
         name: '',
-        images: [],
-        imagesMeta: [],
-        description: '',
-        status: '',
         createdAt: null,
         updatedAt: null
       },
       itemOriginal: {
         uid: uuidv4(),
         name: '',
-        images: [],
-        imagesMeta: [],
-        description: '',
-        status: '',
         createdAt: null,
         updatedAt: null
-      },
-      image: {
-        name: '',
-        url: '',
-        fullPath: '',
-        createdAt: ''
       }
     }
   },
@@ -264,101 +160,18 @@ export default {
         return slugify(string)
       }
     },
-    isDarkColor() {
-      return color => {
-        if (!color) {
-          return
-        }
-        color = color.toString()
-        return isDarkColor(color)
-      }
-    },
-    getMaterialColor() {
-      return (string, isCode = false) => {
-        if (!string) {
-          return
-        }
-        string = string.toString()
-        const { backgroundColor, materialColorName } = materialColorHash(string)
-        return isCode ? backgroundColor : materialColorName.toLowerCase()
-      }
-    },
-    getInitials() {
-      return string => {
-        if (!string) {
-          return
-        }
-        string = string.toString()
-        return initials(string)
-      }
-    },
     isEdited() {
       return this.isEditing && !isEqual(this.item, this.itemOriginal)
-    }
-  },
-  watch: {
-    'item.images': async function(images) {
-      if (images && images.length > 0) {
-        const imagesMeta = await Promise.all(
-          images.map(async (image, i) => ({
-            ...this.item.imagesMeta[i],
-            name: image.name,
-            url: await this.getUrlFromFile(image)
-          }))
-        )
-        this.item.imagesMeta = _cloneDeep(imagesMeta)
-        this.itemOriginal.imagesMeta = _cloneDeep(imagesMeta)
-      } else {
-        this.item.imagesMeta = []
-        this.itemOriginal.imagesMeta = []
-      }
     }
   },
   mounted() {
     this.getItems()
   },
   methods: {
-    async getFileFromUrl(url, name) {
-      try {
-        // Taken from: https://stackoverflow.com/questions/44070437/how-to-get-a-file-or-blob-from-an-url-in-javascript
-        this.$setLoading(true)
-        if (!url) {
-          return
-        }
-        url = url.toString()
-        const response = await fetch(url)
-        const blob = await response.blob()
-        const file = new File([blob], name, {
-          type: blob.type
-        })
-        return file
-      } catch (error) {
-        this.$notify({
-          isError: true,
-          message: error.message
-        })
-      } finally {
-        this.$setLoading(false)
-      }
-    },
-    getUrlFromFile(file) {
-      return new Promise(resolve => {
-        const fileReader = new FileReader()
-        fileReader.readAsDataURL(file)
-        fileReader.addEventListener('load', () => {
-          resolve(fileReader.result)
-        })
-      })
-    },
-
     reset() {
       const item = {
         uid: uuidv4(),
         name: '',
-        images: [],
-        imagesMeta: [],
-        description: '',
-        status: '',
         createdAt: null,
         updatedAt: null
       }
@@ -411,39 +224,16 @@ export default {
     onTriggerAdd() {
       this.isDialog = true
     },
-    async onTriggerEdit(_item) {
-      try {
-        this.$setLoading(true)
-        this.isDialog = true
-        this.isEditing = true
+    onTriggerEdit(item) {
+      this.isDialog = true
+      this.isEditing = true
 
-        const item = _cloneDeep(_item)
-
-        const images = await Promise.all(
-          item.imagesMeta.map(meta =>
-            this.getFileFromUrl(meta.url, meta.name || `${uuidv4()}.jpg`)
-          )
-        )
-        item.images = images
-
-        this.item = _cloneDeep(item)
-        this.itemOriginal = _cloneDeep(item)
-      } catch (error) {
-        this.$notify({
-          isError: true,
-          message: error.message
-        })
-      } finally {
-        this.$setLoading(false)
-      }
+      this.item = _cloneDeep(item)
+      this.itemOriginal = _cloneDeep(item)
     },
     onTriggerDelete(item) {
       this.isDeleting = true
       this.item = _cloneDeep(item)
-    },
-    onTriggerPreview(item) {
-      this.isPreviewing = true
-      this.image = _cloneDeep(item)
     },
 
     onDialogClose() {
@@ -470,30 +260,6 @@ export default {
             createdAt: this.isEditing ? this.item.createdAt : date,
             updatedAt: date
           }
-          if (this.isEdited) {
-            await Promise.all(
-              this.itemOriginal.imagesMeta.map(meta =>
-                storage.ref(meta.fullPath).delete()
-              )
-            )
-          }
-          const imagesMeta = await Promise.all(
-            payload.images.map(async image => {
-              const snap = await storage
-                .ref(this.collection)
-                .child(`${uuidv4()}.jpg`)
-                .put(image)
-              const url = await snap.ref.getDownloadURL()
-              return {
-                url,
-                name: snap.metadata.name,
-                fullPath: snap.metadata.fullPath,
-                createdAt: this.$moment(snap.metadata.timeCreated).toDate()
-              }
-            })
-          )
-          payload.imagesMeta = imagesMeta
-          delete payload.images
           this.isSaved = true
           await db
             .collection(this.collection)
@@ -526,9 +292,6 @@ export default {
           .collection(this.collection)
           .doc(item.uid)
           .delete()
-        await Promise.all(
-          item.imagesMeta.map(meta => storage.ref(meta.fullPath).delete())
-        )
         await this.getItems()
         await this.onDeleteClose()
         await this.$notify({ kind: 'success', message: 'Data is deleted' })
@@ -551,19 +314,6 @@ export default {
       this.isDialog = false
       this.isEditing = false
       this.reset()
-    },
-
-    onPreviewClose() {
-      this.isPreviewing = false
-      this.image = {
-        name: '',
-        url: '',
-        fullPath: '',
-        createdAt: ''
-      }
-    },
-    onPreviewAction() {
-      this.onPreviewClose()
     }
   }
 }
